e49fabb9c658c832db8dc0d97e754f4ce1691f65
ShiMarinho <profa.shirley.marinho@gmail.com>
2019-04-04 17:58:34 +0100

    Investor classes builder pattern implementation

--- src/investor/Factory.java
+++ src/investor/Factory.java
@@ -18,24 +18,25 @@ public class Factory {
     /*
     * to make the method accessible in the main static
      */
-    public static Investor getInvestor(int InvId, double budget, String type) {
+    public static Investor createInvestor(int InvId, double budget, String type) {
 
         switch (type) {
             case "personal":
-                return new PersonalInvestor(InvId, budget);
+                return new PersonalInvestor.PersonalInvestorBuilder(InvId, budget).build();
             case "angel":
-                return new AngelInvestor(InvId, budget);
+                return new AngelInvestor.AngelInvestorBuilder(InvId, budget).build();
             case "peer-to-peer":
-                return new PeerToPeerInvestor(InvId, budget);
+                return new PeerToPeerInvestor.PeerToPeerInvestorBuilder(InvId, budget).build();
             case "venture":
-                return new VentureInvestor(InvId, budget);
+                return new VentureInvestor.VentureInvestorBuilder(InvId, budget).build();
             case "bank investor":
-                return new BankInvestor(InvId, budget);
+                return new BankInvestor.BankInvestorBuilder(InvId, budget).build();
             default:
                 return null;
 
         }
     }
+     
 }
 /*
  * src: https://startupnation.com/sponsored-content/types-investors-startups/
