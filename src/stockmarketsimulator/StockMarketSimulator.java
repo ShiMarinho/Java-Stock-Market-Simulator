0b99a2c1f5f399e911a7ebfa4b2f87cf80d1257b
ShiMarinho <profa.shirley.marinho@gmail.com>
2019-04-26 00:04:27 +0100

    Simulator displays all the Companies and Investors

--- src/stockmarketsimulator/StockMarketSimulator.java
+++ src/stockmarketsimulator/StockMarketSimulator.java
@@ -42,36 +42,23 @@ public class StockMarketSimulator {
      */
     public static void main(String[] args) {
 
-        /*
-         * Factory pattern
-         */
-        System.out.println("Investors \n");
-
-        Investor personal = Factory.createInvestor(1, 100, "personal");
-        System.out.println(personal);
-        Investor angel = Factory.createInvestor(1, 100, "angel");
-        System.out.println(angel);
-        Investor peer = Factory.createInvestor(1, 100, "peer-to-peer");
-        System.out.println(peer);
-        Investor venture = Factory.createInvestor(1, 100, "venture");
-        System.out.println(venture);
-        Investor bankInv = Factory.createInvestor(1, 100, "bank investor");
-        System.out.println(bankInv);
-        
-        /*
-         * Abstractor Factory pattern Companies
-         */
-        System.out.print("\nCompanies\n");
-        Company bank = CompanyFactory.getCompany(new BankFactory(1, 1, 1));
-        System.out.println(bank);
-        Company commodity = CompanyFactory.getCompany(new CommodityFactory(1, 1, 1));
-        System.out.println(commodity);
-        Company industrial = CompanyFactory.getCompany(new IndustrialFactory(1, 1, 1));
-        System.out.println(industrial);
-        Company food = CompanyFactory.getCompany(new FoodBeverageFactory(1, 1, 1));
-        System.out.println(food);
-        Company tech = CompanyFactory.getCompany(new TechFactory(1, 1, 1));
-        System.out.println(tech);
+      
+        new StockMarketSimulator();
     }
+    
+    public StockMarketSimulator(){
+         
+
+        Simulator simulator = Simulator.getInstance();
+        simulator.addTrade(SetUp.getCompanies(), SetUp.getInvestors());
+        simulator.getTrade(SetUp.getInvestors());
+        //System.out.println(simulator.getTrade(SetUp.getInvestors()));
+
+        Transaction tr = new Transaction(1);
+
+        Simulator sim = tr.getSimulator();
+       System.out.println(sim.getTrade());
+    }
+    
 
 }
